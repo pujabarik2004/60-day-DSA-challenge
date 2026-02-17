8ba5e70e87fa3a16f5fd9fb12bc23c4f44b369de
pujabarik2004 <pooja20040626@gmail.com>
2026-02-17 20:38:45 +0530

    Implement binary search for smallest letter greater than target

---
+++ Arrays/smallest letter greater than target.py
@@ -0,0 +1,11 @@
+class Solution(object):
+  def smallestgreaterthantarget(selfs,nums,target):
+    low=0
+    high=len(nums)-1
+    while low<=high:
+      mid=(low+high)//2
+      if nums[mid]<target:
+        low=mid+1
+      else:
+        high=mid-1
+    return nums[low%len(nums)]
